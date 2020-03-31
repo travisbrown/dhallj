f2055d1cbb5e29732643e3d13473f69e40825a99
Tim Spence <timothywspence@gmail.com>
2020-03-31 15:32:13 +0100

    Initial implementation of CORS compliance checking

---
+++ imports/src/main/scala/org/dhallj/imports/CORSComplianceCheck.scala
@@ -0,0 +1,45 @@
+package org.dhallj.imports
+
+import java.net.URI
+
+import cats.effect.Sync
+import org.http4s.Headers
+import org.http4s.headers.`Access-Control-Allow-Origin`
+
+object CORSComplianceCheck {
+
+  def apply[F[_]](parent: ImportContext, child: ImportContext, headers: Headers)(implicit F: Sync[F]): F[Unit] =
+    parent match {
+      case Remote(uri) =>
+        child match {
+          case Remote(uri2) =>
+            if (sameOrigin(uri, uri2))
+              F.unit
+            else
+              headers
+                .get(`Access-Control-Allow-Origin`)
+                .fold(
+                  F.raiseError[Unit](
+                    new RuntimeException(
+                      s"CORS compliance failure - No Access-Control-Allow-Origin header for import $uri2 from $uri"
+                    )
+                  )
+                ) { h =>
+                  if (h.value.trim == "*" || sameOrigin(new URI(h.value), uri))
+                    F.unit
+                  else
+                    F.raiseError(
+                      new RuntimeException(
+                        s"CORS compliance failure - ${h.value.trim} is invalid for import $uri2 from $uri"
+                      )
+                    )
+                }
+          case _ => F.unit
+        }
+      case _ => F.unit
+    }
+
+  private def sameOrigin(uri: URI, uri2: URI): Boolean =
+    uri.getScheme == uri2.getScheme && uri.getAuthority == uri2.getAuthority && uri.getPort == uri2.getPort
+
+}
---
+++ imports/src/test/scala/org/dhallj/imports/CORSComplianceCheckSuite.scala
@@ -0,0 +1,70 @@
+package org.dhallj.imports
+
+import java.net.URI
+import java.nio.file.Paths
+
+import cats.effect.IO
+import munit.FunSuite
+import org.http4s.{Header, Headers}
+
+class CORSComplianceCheckSuite extends FunSuite {
+
+  val fooOrigin = new URI("http://foo.org/foo.dhall")
+  val fooOrigin8080 = new URI("http://foo.org:8080/foo.dhall")
+  val fooOrigin2 = new URI("http://foo.org/baz.dhall")
+  val barOrigin = new URI("http://bar.org/bar.dhall")
+
+  val localPath = Paths.get("/foo/bar.dhall")
+
+  test("Remote - same origin") {
+    CORSComplianceCheck[IO](Remote(fooOrigin), Remote(fooOrigin2), Headers.empty).unsafeRunSync()
+  }
+
+  test("Remote - different origin, allow *") {
+    CORSComplianceCheck[IO](Remote(fooOrigin),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "*"))).unsafeRunSync()
+  }
+
+  test("Remote - different origin, allow parent authority") {
+    CORSComplianceCheck[IO](Remote(fooOrigin),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "http://foo.org"))).unsafeRunSync()
+  }
+
+  test("Remote - different origin".fail) {
+    CORSComplianceCheck[IO](Remote(fooOrigin), Remote(barOrigin), Headers.empty).unsafeRunSync()
+  }
+
+  test("Remote - different origin, cors parent different authority".fail) {
+    CORSComplianceCheck[IO](Remote(fooOrigin),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "http://bar.org"))).unsafeRunSync()
+  }
+
+  test("Remote - different origin, cors parent different scheme".fail) {
+    CORSComplianceCheck[IO](Remote(fooOrigin),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "https://foo.org"))).unsafeRunSync()
+  }
+
+  test("Remote - different origin, cors parent different port".fail) {
+    CORSComplianceCheck[IO](Remote(fooOrigin),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "http://foo.org:8080"))).unsafeRunSync()
+  }
+
+  test("Remote - different origin, cors parent different port 2".fail) {
+    CORSComplianceCheck[IO](Remote(fooOrigin8080),
+                            Remote(barOrigin),
+                            Headers.of(Header("Access-Control-Allow-Origin", "http://foo.org"))).unsafeRunSync()
+  }
+
+  test("Local") {
+    CORSComplianceCheck[IO](Local(localPath), Remote(fooOrigin2), Headers.empty).unsafeRunSync()
+  }
+
+  test("Env") {
+    CORSComplianceCheck[IO](Env("foo"), Remote(fooOrigin2), Headers.empty).unsafeRunSync()
+  }
+}
