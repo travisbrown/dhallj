c65a15bc9d55c307dd3746531c5f6bdbbe43de5b
Tim Spence <timothywspence@gmail.com>
2020-04-01 11:42:27 +0100

    Update sample main

--- imports/src/main/scala/org/dhallj/imports/Main.scala
+++ imports/src/main/scala/org/dhallj/imports/Main.scala
@@ -1,16 +1,30 @@
 package org.dhallj.imports
 
-import org.dhallj.parser.Dhall.parse
+import java.net.URI
+import java.nio.file.Path
+
+import cats.effect.{ExitCode, IO, IOApp}
 import org.dhallj.core.{Expr, Import}
-import java.nio.file.{Path, Paths}
-import org.dhallj.imports._
-import cats.effect.IO
-import cats.implicits._
+import org.dhallj.parser.Dhall.parse
+import org.http4s.client._
+import org.http4s.client.blaze._
 
-object Main extends App {
+import scala.concurrent.ExecutionContext.global
 
-  def withImport(path: Path, code: String): Expr = Expr.makeLet("x", null, Expr.makeLocalImport(path, Import.Mode.CODE, null), parse(code))
+object Main extends IOApp {
 
-  (withImport(Paths.get("/tmp/foo.dhall"), "[x, x, x]").resolveImports[IO] >>= (v => IO(println(v.normalize())))).unsafeRunSync
+  override def run(args: List[String]): IO[ExitCode] = BlazeClientBuilder[IO](global).resource.use { client =>
+    implicit val c: Client[IO] = client
+
+    for {
+      e1 <- IO.pure(withRemoteImport(new URI("https://raw.githubusercontent.com/dhall-lang/dhall-lang/master/Prelude/List/any"), "x Natural Natural/even [ 2, 3, 5 ]"))
+      e2 <- e1.resolveImports[IO]
+      _ <- IO(println(e2.normalize))
+    } yield ExitCode.Success
+  }
+
+  def withLocalImport(path: Path, code: String): Expr = Expr.makeLet("x", null, Expr.makeLocalImport(path, Import.Mode.CODE, null), parse(code))
+
+  def withRemoteImport(uri: URI, code: String): Expr = Expr.makeLet("x", null, Expr.makeRemoteImport(uri, Import.Mode.CODE, null), parse(code))
 
 }
