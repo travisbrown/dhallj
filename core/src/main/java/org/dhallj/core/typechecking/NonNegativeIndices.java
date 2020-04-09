6c52b18a9cf0a4dfd8f5df29264f9b9cbacc9a19
Travis Brown <travisrobertbrown@gmail.com>
2020-04-09 05:48:29 -0500

    Make index check package-private

--- core/src/main/java/org/dhallj/core/typechecking/NonNegativeIndices.java
+++ core/src/main/java/org/dhallj/core/typechecking/NonNegativeIndices.java
@@ -6,7 +6,7 @@ import org.dhallj.core.Expr;
 import org.dhallj.core.Vis;
 import org.dhallj.core.visitor.PropertyVis;
 
-public final class NonNegativeIndices extends PropertyVis {
+final class NonNegativeIndices extends PropertyVis {
   public static final Vis<Boolean> instance = new NonNegativeIndices();
 
   @Override
