1159a80ca139d9900f39df306a2ce4a7e835fed4
Travis Brown <travisrobertbrown@gmail.com>
2020-04-10 12:34:34 -0500

    Add toString check that often fails right now

--- tests/src/test/scala/org/dhallj/tests/ToStringSuite.scala
+++ tests/src/test/scala/org/dhallj/tests/ToStringSuite.scala
@@ -1,6 +1,6 @@
 package org.dhallj.tests
 
-import munit.ScalaCheckSuite
+import munit.{Ignore, ScalaCheckSuite}
 import org.dhallj.core.Expr
 import org.dhallj.parser.DhallParser
 import org.dhallj.testing.WellTypedExpr
@@ -8,7 +8,15 @@ import org.dhallj.testing.instances._
 import org.scalacheck.{Arbitrary, Prop}
 
 class ToStringSuite extends ScalaCheckSuite() {
-  property("toString produces parseable code") {
-    Prop.forAll((expr: WellTypedExpr) => DhallParser.parse(clue(expr.value.toString)) == expr.value)
+  property("toString produces parseable code given well-typed values") {
+    Prop.forAll { (expr: WellTypedExpr) =>
+      DhallParser.parse(clue(expr.value.toString)) == expr.value
+    }
+  }
+
+  property("toString produces parseable code".tag(Ignore)) {
+    Prop.forAll { (expr: Expr) =>
+      DhallParser.parse(clue(expr.toString)) == expr
+    }
   }
 }
