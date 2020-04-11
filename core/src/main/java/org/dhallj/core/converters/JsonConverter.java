b010e197dd0246e15c3f8d891d4fb1e7c3fe94aa
Travis Brown <travisrobertbrown@gmail.com>
2020-04-11 09:22:34 -0500

    Add sketch of JSON converter

---
+++ core/src/main/java/org/dhallj/core/converters/JsonConverter.java
@@ -0,0 +1,124 @@
+package org.dhallj.core.converters;
+
+import java.math.BigInteger;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import org.dhallj.cbor.Writer;
+import org.dhallj.core.Expr;
+import org.dhallj.core.Operator;
+import org.dhallj.core.Source;
+import org.dhallj.core.Visitor;
+
+public final class JsonConverter extends Visitor.Constant<Boolean> {
+  private final JsonHandler handler;
+
+  public JsonConverter(JsonHandler handler) {
+    super(false);
+    this.handler = handler;
+  }
+
+  public static final String toCompactString(Expr expr) {
+    JsonHandler.CompactStringPrinter handler = new JsonHandler.CompactStringPrinter();
+    boolean isConverted = expr.accept(new JsonConverter(handler));
+    if (isConverted) {
+      return handler.toString();
+    } else {
+      return null;
+    }
+  }
+
+  public boolean sortFields() {
+    return false;
+  }
+
+  public Boolean onNatural(Expr self, BigInteger value) {
+    this.handler.onNumber(value.toString());
+    return true;
+  }
+
+  public Boolean onInteger(Expr self, BigInteger value) {
+    this.handler.onNumber(value.toString());
+    return true;
+  }
+
+  public Boolean onDouble(Expr self, double value) {
+    this.handler.onDouble(value);
+    return true;
+  }
+
+  public Boolean onBuiltIn(Expr self, String name) {
+    if (name.equals("True")) {
+      this.handler.onBoolean(true);
+      return true;
+    } else if (name.equals("False")) {
+      this.handler.onBoolean(false);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public Boolean onText(String[] parts, List<Boolean> interpolated) {
+    if (parts.length == 1) {
+      this.handler.onString(parts[0]);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean prepareNonEmptyList(int size) {
+    this.handler.onArrayStart();
+    return true;
+  }
+
+  public boolean prepareNonEmptyListElement(int index) {
+    if (index > 0) {
+      this.handler.onArrayElementGap();
+    }
+    return true;
+  }
+
+  public Boolean onNonEmptyList(List<Boolean> values) {
+    for (boolean value : values) {
+      if (!value) {
+        return false;
+      }
+    }
+    this.handler.onArrayEnd();
+    return true;
+  }
+
+  public Boolean onEmptyList(Boolean type) {
+    this.handler.onArrayStart();
+    this.handler.onArrayEnd();
+    return true;
+  }
+
+  public boolean prepareRecord(int size) {
+    this.handler.onObjectStart();
+    return true;
+  }
+
+  public boolean prepareRecordField(String name, Expr type, int index) {
+    if (index > 0) {
+      this.handler.onObjectFieldGap();
+    }
+    this.handler.onObjectField(name);
+    return true;
+  }
+
+  public Boolean onRecord(final List<Entry<String, Boolean>> fields) {
+    for (Entry<String, Boolean> field : fields) {
+      if (!field.getValue()) {
+        return false;
+      }
+    }
+    this.handler.onObjectEnd();
+    return true;
+  }
+}
---
+++ core/src/main/java/org/dhallj/core/converters/JsonHandler.java
@@ -0,0 +1,145 @@
+package org.dhallj.core.converters;
+
+import java.io.PrintWriter;
+
+public interface JsonHandler {
+  void onNull();
+
+  void onBoolean(boolean value);
+
+  void onNumber(String value);
+
+  void onDouble(double value);
+
+  void onString(String value);
+
+  void onArrayStart();
+
+  void onArrayEnd();
+
+  void onArrayElementGap();
+
+  void onObjectStart();
+
+  void onObjectEnd();
+
+  void onObjectField(String name);
+
+  void onObjectFieldGap();
+
+  public static final class CompactPrinter implements JsonHandler {
+    private final PrintWriter writer;
+
+    public CompactPrinter(PrintWriter writer) {
+      this.writer = writer;
+    }
+
+    public void onNull() {
+      this.writer.print("null");
+    }
+
+    public void onBoolean(boolean value) {
+      this.writer.print(value);
+    }
+
+    public void onNumber(String value) {
+      this.writer.print(value);
+    }
+
+    public void onDouble(double value) {
+      this.writer.print(value);
+    }
+
+    public void onString(String value) {
+      this.writer.printf("\"%s\"", value);
+    }
+
+    public void onArrayStart() {
+      this.writer.print("[");
+    }
+
+    public void onArrayEnd() {
+      this.writer.print("]");
+    }
+
+    public void onArrayElementGap() {
+      this.writer.print(",");
+    }
+
+    public void onObjectStart() {
+      this.writer.print("{");
+    }
+
+    public void onObjectEnd() {
+      this.writer.print("}");
+    }
+
+    public void onObjectField(String name) {
+      this.writer.printf("\"%s\":", name);
+    }
+
+    public void onObjectFieldGap() {
+      this.writer.print(",");
+    }
+  }
+
+  public static final class CompactStringPrinter implements JsonHandler {
+    private final StringBuilder builder;
+
+    public CompactStringPrinter() {
+      this.builder = new StringBuilder();
+    }
+
+    public String toString() {
+      return this.builder.toString();
+    }
+
+    public void onNull() {
+      this.builder.append("null");
+    }
+
+    public void onBoolean(boolean value) {
+      this.builder.append(value);
+    }
+
+    public void onNumber(String value) {
+      this.builder.append(value);
+    }
+
+    public void onDouble(double value) {
+      this.builder.append(value);
+    }
+
+    public void onString(String value) {
+      this.builder.append(String.format("\"%s\"", value));
+    }
+
+    public void onArrayStart() {
+      this.builder.append("[");
+    }
+
+    public void onArrayEnd() {
+      this.builder.append("]");
+    }
+
+    public void onArrayElementGap() {
+      this.builder.append(",");
+    }
+
+    public void onObjectStart() {
+      this.builder.append("{");
+    }
+
+    public void onObjectEnd() {
+      this.builder.append("}");
+    }
+
+    public void onObjectField(String name) {
+      this.builder.append(String.format("\"%s\":", name));
+    }
+
+    public void onObjectFieldGap() {
+      this.builder.append(",");
+    }
+  }
+}
