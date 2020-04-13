c4df7bb510462450984309a6fa84ca3f40f1dca5
Tim Spence <timothywspence@gmail.com>
2020-04-13 23:14:45 +0100

    Enable ignored type inference tests

--- tests/src/test/scala/org/dhallj/tests/acceptance/AcceptanceSuites.scala
+++ tests/src/test/scala/org/dhallj/tests/acceptance/AcceptanceSuites.scala
@@ -25,7 +25,6 @@ class TypeCheckingSimpleSuite extends TypeCheckingSuite("type-inference/success/
 class TypeCheckingUnitSuite extends TypeCheckingSuite("type-inference/success/unit")
 class TypeCheckingRegressionSuite extends TypeCheckingSuite("type-inference/success/regression")
 class TypeCheckingOtherSuite extends TypeCheckingSuite("type-inference/success") {
-  override def ignored = Set("CacheImports", "CacheImportsCanonicalize")
   override def slow = Set("prelude")
 }
 class TypeCheckingFailureUnitSuite extends TypeCheckingFailureSuite("type-inference/failure/unit")
