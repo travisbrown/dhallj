8871eb73d31eace7ae018faea4e9b07132b826af
Travis Brown <travisrobertbrown@gmail.com>
2020-04-15 11:36:11 +0200

    Add new Cats integration module

---
+++ modules/cats/src/test/scala/org/dhallj/cats/LiftVisitorSuite.scala
@@ -0,0 +1,19 @@
+package org.dhallj.cats
+
+import cats.Applicative
+import cats.instances.option._
+import munit.ScalaCheckSuite
+import org.dhallj.core.Expr
+import org.dhallj.parser.DhallParser
+import org.dhallj.testing.instances._
+import org.scalacheck.{Arbitrary, Prop}
+
+class ToStringSuite extends ScalaCheckSuite {
+  property("LiftVisitor with no overrides is pure") {
+    Prop.forAll { (expr: Expr) =>
+      val lift = new LiftVisitor[Option](Applicative[Option])
+
+      expr.accept(lift) == Some(expr)
+    }
+  }
+}
