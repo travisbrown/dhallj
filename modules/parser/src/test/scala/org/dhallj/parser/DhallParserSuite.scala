9812741741c56a6075976f7f3223223aedf80646
Travis Brown <travisrobertbrown@gmail.com>
2020-04-16 21:42:12 +0200

    Fix parser issue on type annotations in e.g. empty list type annotations (#36)
    * Add tests for #1
    
    * Fix annotations on annotations

--- modules/parser/src/test/scala/org/dhallj/parser/DhallParserSuite.scala
+++ modules/parser/src/test/scala/org/dhallj/parser/DhallParserSuite.scala
@@ -8,12 +8,46 @@ import org.dhallj.core.Expr
 import org.dhallj.core.Expr.ImportMode
 
 class DhallParserSuite extends FunSuite() {
-  test("parse empty list with annotation on element type".tag(Ignore)) {
+  test("parse empty list with annotation on element type") {
     val expected = Expr.makeEmptyListLiteral(
-      Expr.makeApplication(Expr.Constants.LIST, Expr.makeAnnotated(Expr.Constants.NATURAL, Expr.Constants.TYPE))
+      Expr.makeAnnotated(Expr.makeApplication(Expr.Constants.LIST, Expr.Constants.NATURAL), Expr.Constants.TYPE)
     )
+    // Output from dhall-haskell.
+    val expectedBytes: Array[Byte] = Array(-126, 24, 28, -125, 24, 26, -125, 0, 100, 76, 105, 115, 116, 103, 78, 97,
+      116, 117, 114, 97, 108, 100, 84, 121, 112, 101)
+    val parsed = DhallParser.parse("[]: List Natural: Type")
 
-    assert(DhallParser.parse("[]: List Natural: Type") == expected)
+    assert(parsed == expected)
+    assert(parsed.getEncodedBytes.sameElements(expectedBytes))
+  }
+
+  test("parse toMap with empty record with annotation on type") {
+    // Output from dhall-haskell.
+    val expectedBytes: Array[Byte] = Array(-125, 24, 27, -126, 7, -96, -125, 24, 26, -125, 0, 100, 76, 105, 115, 116,
+      -126, 7, -94, 102, 109, 97, 112, 75, 101, 121, 100, 84, 101, 120, 116, 104, 109, 97, 112, 86, 97, 108, 117, 101,
+      100, 66, 111, 111, 108, 100, 84, 121, 112, 101)
+    val parsed = DhallParser.parse("toMap {}: List { mapKey : Text, mapValue: Bool }: Type")
+
+    assert(parsed.getEncodedBytes.sameElements(expectedBytes))
+  }
+
+  test("parse toMap with empty non-record with annotation on type") {
+    // Output from dhall-haskell.
+    val expectedBytes: Array[Byte] = Array(-125, 24, 27, -126, 8, -95, 97, 97, -11, -125, 24, 26, -125, 0, 100, 76, 105,
+      115, 116, -126, 7, -94, 102, 109, 97, 112, 75, 101, 121, 100, 84, 101, 120, 116, 104, 109, 97, 112, 86, 97, 108,
+      117, 101, 100, 66, 111, 111, 108, 100, 84, 121, 112, 101)
+    val parsed = DhallParser.parse("toMap {a=True}: List { mapKey : Text, mapValue: Bool }: Type")
+
+    assert(parsed.getEncodedBytes.sameElements(expectedBytes))
+  }
+
+  test("parse merge with annotation on type") {
+    // Output from dhall-haskell.
+    val expectedBytes: Array[Byte] = Array(-124, 6, -126, 8, -95, 97, 97, -126, 15, 1, -125, 9, -126, 11, -95, 97, 97,
+      -10, 97, 97, -125, 24, 26, 103, 78, 97, 116, 117, 114, 97, 108, 100, 84, 121, 112, 101)
+    val parsed = DhallParser.parse("merge {a=1} <a>.a: Natural: Type")
+
+    assert(clue(parsed.getEncodedBytes).sameElements(clue(expectedBytes)))
   }
 
   test("parse IPv6 address") {
