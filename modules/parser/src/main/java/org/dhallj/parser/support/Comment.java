be94be84ad3d580532fba52951d2a4929c931345
Travis Brown <travisrobertbrown@gmail.com>
2020-04-16 04:48:23 -0500

    Some work on the JavaCC parser

--- modules/parser/src/main/java/org/dhallj/parser/support/Comment.java
+++ modules/parser/src/main/java/org/dhallj/parser/support/Comment.java
@@ -1,15 +1,38 @@
 package org.dhallj.parser.support;
 
-final class Comment extends Positioned {
+final class Comment {
   private final String content;
+  private final int beginLine;
+  private final int beginColumn;
+  private final int endLine;
+  private final int endColumn;
 
   Comment(String content, int beginLine, int beginColumn, int endLine, int endColumn) {
-    super(beginLine, beginColumn, endLine, endColumn);
     this.content = content;
+    this.beginLine = beginLine;
+    this.beginColumn = beginColumn;
+    this.endLine = endLine;
+    this.endColumn = endColumn;
   }
 
   public String getContent() {
     return this.content;
+  }
+
+  public final int getBeginLine() {
+    return this.beginLine;
+  }
+
+  public final int getBeginColumn() {
+    return this.beginColumn;
+  }
+
+  public final int getEndLine() {
+    return this.endLine;
+  }
+
+  public final int getEndColumn() {
+    return this.endColumn;
   }
 
   public String toString() {
