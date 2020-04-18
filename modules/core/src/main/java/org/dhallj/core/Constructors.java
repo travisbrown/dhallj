f336e11a1cbc39ddc38cef76d7395783b0a0372c
Travis Brown <travisrobertbrown@gmail.com>
2020-04-18 19:40:32 +0200

    Remove unused imports

--- modules/core/src/main/java/org/dhallj/core/Constructors.java
+++ modules/core/src/main/java/org/dhallj/core/Constructors.java
@@ -3,8 +3,6 @@ package org.dhallj.core;
 import java.math.BigInteger;
 import java.net.URI;
 import java.nio.file.Path;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map.Entry;
 
 /**
