44e610ef2197d333dac45592f266cda03bd062de
Tim Spence <timothywspence@gmail.com>
2020-04-20 12:12:35 +0100

    Apply formatting

--- modules/imports/src/main/scala/org/dhallj/imports/ResolveImportsVisitor.scala
+++ modules/imports/src/main/scala/org/dhallj/imports/ResolveImportsVisitor.scala
@@ -24,7 +24,9 @@ import org.http4s.{EntityDecoder, Request}
 import scala.collection.mutable.{Map => MMap}
 
 //TODO proper error handling
-private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: ImportsCache[F], semiSemanticCache: ImportsCache[F], parents: List[ImportContext])(
+private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: ImportsCache[F],
+                                                                 semiSemanticCache: ImportsCache[F],
+                                                                 parents: List[ImportContext])(
   implicit Client: Client[F],
   F: Sync[F]
 ) extends LiftVisitor[F](F) {
@@ -97,17 +99,16 @@ private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: 
         F.raiseError[Unit](new ResolutionFailure(s"Cyclic import - $imp is already imported in chain $parents"))
       else F.unit
 
-    def importLocation(imp: ImportContext): F[Expr] = {
+    def importLocation(imp: ImportContext): F[Expr] =
       imp match {
         case Local(path) => makeLocation("Local", path.toString)
         // Cannot support this and remain spec-compliant as result type must be <Local Text | Remote Text | Environment Text | Missing>
         case Classpath(path) =>
           F.raiseError(new ResolutionFailure("Importing classpath as location is not supported"))
         case Remote(uri, _) => makeLocation("Remote", uri.toString)
-        case Env(value) => makeLocation("Environment", value)
-        case Missing => F.pure(Expr.makeFieldAccess(Expr.Constants.LOCATION_TYPE, "Missing"))
+        case Env(value)     => makeLocation("Environment", value)
+        case Missing        => F.pure(Expr.makeFieldAccess(Expr.Constants.LOCATION_TYPE, "Missing"))
       }
-    }
 
     def makeLocation(field: String, value: String): F[Expr] =
       F.pure(
@@ -122,21 +123,24 @@ private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: 
 
     def loadWithSemanticCache(imp: ImportContext, mode: ImportMode, hash: Array[Byte]): F[Expr] =
       if (hash == null) loadWithSemiSemanticCache(imp, mode, hash)
-      else for {
-        cached <- semanticCache.get(hash)
-        e <- cached match {
-          case Some(bs) => for {
-            _ <- checkHashesMatch(bs, hash)
-            e <- F.delay(Decode.decode(bs))
-          } yield e
-          case None => for {
-            e <- loadWithSemiSemanticCache(imp, mode, hash)
-            bs = e.alphaNormalize.getEncodedBytes
-            _ <- checkHashesMatch(bs, hash)
-            _ <- semanticCache.put(hash, bs)
-          } yield e
-        }
-      } yield e
+      else
+        for {
+          cached <- semanticCache.get(hash)
+          e <- cached match {
+            case Some(bs) =>
+              for {
+                _ <- checkHashesMatch(bs, hash)
+                e <- F.delay(Decode.decode(bs))
+              } yield e
+            case None =>
+              for {
+                e <- loadWithSemiSemanticCache(imp, mode, hash)
+                bs = e.alphaNormalize.getEncodedBytes
+                _ <- checkHashesMatch(bs, hash)
+                _ <- semanticCache.put(hash, bs)
+              } yield e
+          }
+        } yield e
 
     def fetch(imp: ImportContext): F[String] = imp match {
       case Env(value) =>
@@ -176,47 +180,52 @@ private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: 
     }
 
     def loadWithSemiSemanticCache(imp: ImportContext, mode: ImportMode, hash: Array[Byte]): F[Expr] = mode match {
-      case ImportMode.LOCATION => imp match {
-        case Local(path) => makeLocation("Local", path.toString)
-        // Cannot support this and remain spec-compliant as result type must be <Local Text | Remote Text | Environment Text | Missing>
-        case Classpath(path) =>
-          F.raiseError(new ResolutionFailure("Importing classpath as location is not supported"))
-        case Remote(uri, _) => makeLocation("Remote", uri.toString)
-        case Env(value) => makeLocation("Environment", value)
-        case Missing => F.pure(Expr.makeFieldAccess(Expr.Constants.LOCATION_TYPE, "Missing"))
-      }
-      case ImportMode.RAW_TEXT => for {
-        text <- fetch(imp)
-      } yield Expr.makeTextLiteral(text)
-      case ImportMode.CODE => for {
-        text <- fetch(imp)
-        parsed <- F.delay(DhallParser.parse(text))
-        resolved <- {
-          val v = ResolveImportsVisitor[F](semanticCache, semiSemanticCache, imp :: parents)
-          v.duplicateImportsCache = this.duplicateImportsCache
-          parsed.accept(v)
+      case ImportMode.LOCATION =>
+        imp match {
+          case Local(path) => makeLocation("Local", path.toString)
+          // Cannot support this and remain spec-compliant as result type must be <Local Text | Remote Text | Environment Text | Missing>
+          case Classpath(path) =>
+            F.raiseError(new ResolutionFailure("Importing classpath as location is not supported"))
+          case Remote(uri, _) => makeLocation("Remote", uri.toString)
+          case Env(value)     => makeLocation("Environment", value)
+          case Missing        => F.pure(Expr.makeFieldAccess(Expr.Constants.LOCATION_TYPE, "Missing"))
         }
-        semiHash = MessageDigest.getInstance("SHA-256").digest(resolved.getEncodedBytes)
-        cached <- semiSemanticCache.get(semiHash)
-        expr <- cached match {
-          case Some(bs) => F.delay(Decode.decode(bs))
-          case None => for {
-            _ <- F.delay(typeCheck(resolved))
-            //TODO substitutions here?
-            normalized <- F.delay(resolved.normalize)
-            _ <- semiSemanticCache.put(semiHash, normalized.getEncodedBytes)
-          } yield normalized
-        }
-      } yield expr
+      case ImportMode.RAW_TEXT =>
+        for {
+          text <- fetch(imp)
+        } yield Expr.makeTextLiteral(text)
+      case ImportMode.CODE =>
+        for {
+          text <- fetch(imp)
+          parsed <- F.delay(DhallParser.parse(text))
+          resolved <- {
+            val v = ResolveImportsVisitor[F](semanticCache, semiSemanticCache, imp :: parents)
+            v.duplicateImportsCache = this.duplicateImportsCache
+            parsed.accept(v)
+          }
+          semiHash = MessageDigest.getInstance("SHA-256").digest(resolved.getEncodedBytes)
+          cached <- semiSemanticCache.get(semiHash)
+          expr <- cached match {
+            case Some(bs) => F.delay(Decode.decode(bs))
+            case None =>
+              for {
+                _ <- F.delay(typeCheck(resolved))
+                //TODO substitutions here?
+                normalized <- F.delay(resolved.normalize)
+                _ <- semiSemanticCache.put(semiHash, normalized.getEncodedBytes)
+              } yield normalized
+          }
+        } yield expr
     }
 
     def resolve(imp: ImportContext, mode: ImportMode, hash: Array[Byte]): F[Expr] =
       if (duplicateImportsCache.contains(imp))
         F.delay(duplicateImportsCache.get(imp).get)
-      else for {
-        e <- loadWithSemanticCache(imp, mode, hash)
-        _ <- F.delay(duplicateImportsCache.put(imp, e))
-      } yield e
+      else
+        for {
+          e <- loadWithSemanticCache(imp, mode, hash)
+          _ <- F.delay(duplicateImportsCache.put(imp, e))
+        } yield e
 
     def importNonLocation(imp: ImportContext, mode: ImportMode, hash: Array[Byte]) =
       for {
@@ -234,10 +243,11 @@ private[dhallj] case class ResolveImportsVisitor[F[_] <: AnyRef](semanticCache: 
 
 object ResolveImportsVisitor {
 
-  def mkVisitor[F[_] <: AnyRef : Sync : Client]: F[ResolveImportsVisitor[F]] =
+  def mkVisitor[F[_] <: AnyRef: Sync: Client]: F[ResolveImportsVisitor[F]] =
     (Caching.mkImportsCache[F]("dhall"), Caching.mkImportsCache[F]("dhallj")).mapN((c, c2) => mkVisitor(c, c2))
 
-  def mkVisitor[F[_] <: AnyRef : Sync : Client](semanticCache: ImportsCache[F], semiSemanticCache: ImportsCache[F]): ResolveImportsVisitor[F] =
+  def mkVisitor[F[_] <: AnyRef: Sync: Client](semanticCache: ImportsCache[F],
+                                              semiSemanticCache: ImportsCache[F]): ResolveImportsVisitor[F] =
     ResolveImportsVisitor(semanticCache, semiSemanticCache, Nil)
 
   sealed trait ImportContext
