901a7b0ba59eabe6640302b241c19c9a53f85ac4
Tim Spence <timothywspence@gmail.com>
2020-04-21 17:47:36 +0100

    Allow changing the root directory for resolving imports
    We can now do
    `Resolver.resolve(expr)`
    or
    `Resolver.resolveRelativeTo(Paths.get("/tmp"))(expr)`

---
+++ tests/src/test/scala/org/dhallj/tests/ImportResolutionSuite.scala
@@ -0,0 +1,36 @@
+package org.dhallj.tests
+
+import java.nio.file.{Path, Paths}
+
+import cats.effect.{ContextShift, IO, Resource}
+import munit.FunSuite
+import org.dhallj.core.Expr
+import org.dhallj.imports.syntax._
+import org.dhallj.parser.DhallParser.parse
+import org.http4s.client._
+import org.http4s.client.blaze._
+
+import scala.concurrent.ExecutionContext.global
+
+class ImportResolutionSuite extends FunSuite {
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+
+  implicit val client: Resource[IO, Client[IO]] = BlazeClientBuilder[IO](global).resource
+
+  test("Resolve with different base directory") {
+    //Path inside dhall-lang submodule
+    val expr = parse("let x = ./success/prelude/Bool/and/0B.dhall in x")
+    val expected = parse("Bool").normalize
+
+    assert(resolveRelativeTo(Paths.get("./dhall-lang/tests/type-inference"))(expr) == expected)
+  }
+
+  private def resolveRelativeTo(relativeTo: Path)(e: Expr): Expr =
+    client.use { c =>
+      implicit val http: Client[IO] = c
+
+      e.resolveImportsRelativeTo[IO](relativeTo).map(_.normalize)
+    }.unsafeRunSync
+
+}
